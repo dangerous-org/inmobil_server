ffd6549ff93cac333266e1f563ee816a7d9f4285
Andres <andresdavidpachecocuadro@gmail.com>
2024-04-28 13:26:50 -0500

    se completó el endpoint para validar la authenticacion de un usuario desde el front end

--- src/infrastructure/routes/user.routes.js
+++ src/infrastructure/routes/user.routes.js
@@ -6,15 +6,20 @@ import {
 } from "../../application/controllers/user.controller.js";
 import emailValidator from "../middlewares/email.validator.js";
 import userNameValidator from "../middlewares/username.validator.js";
-import authValidator from "../middlewares/auth.validator.js";
 import passwordValidator from "../middlewares/password.validator.js";
 
 const userRoutes = Router();
 
-userRoutes.post("/create", passwordValidator, userNameValidator, emailValidator, CreateUser);
+userRoutes.post(
+  "/create",
+  passwordValidator,
+  userNameValidator,
+  emailValidator,
+  CreateUser
+);
 
 userRoutes.post("/login", passwordValidator, emailValidator, LoginUser);
 
-userRoutes.post("/authenticateUser", authValidator, AuthenticateUser);
+userRoutes.post("/authenticateUser", AuthenticateUser);
 
 export default userRoutes;
