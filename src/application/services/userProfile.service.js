5bb7f29ddf7865b86777bcb814b928cabb2e5956
andresdvx <andresdavidpachecocuadro@gmail.com>
2024-05-24 15:39:23 -0500

    servicio y funcion upload foto modificada para que se actuyalicen las fotos del post

--- src/application/services/userProfile.service.js
+++ src/application/services/userProfile.service.js
@@ -12,9 +12,28 @@ export const createUserProfileService = async (info) => {
   return UserProfile;
 };
 
-export const updateUserProfileService = async (userId, info, picture) => {
+export const updateUserProfileService = async (userId, info, files) => {
   const { names, lastName, dni, phoneNumber, location, biography, birthDate } =
     info;
+
+  let picture = null;
+
+  if (files != null || files != undefined) {
+    picture = files.picture;
+  }
+
+  if (!picture) {
+    await updateUserProfileRepository(userId, {
+      names,
+      lastName,
+      dni,
+      phoneNumber,
+      location,
+      biography,
+      birthDate,
+    });
+    return getUserProfileRepository(userId);
+  }
 
   const oldUserProfile = await getUserProfileRepository(userId);
 
@@ -34,9 +53,8 @@ export const updateUserProfileService = async (userId, info, picture) => {
     picture: newPicture,
   });
 
-  return UserProfile;
+  return getUserProfileRepository(userId);
 };
-
 
 export const getUserProfileService = async (userName) => {
   const [User] = await findByUserNameRepository(userName);
@@ -45,6 +63,6 @@ export const getUserProfileService = async (userName) => {
 
   const UserProfile = await getUserProfileRepository(User._id);
   const Posts = await getPostByUserRepository(User._id);
-  
+
   return { UserProfile, Posts };
 };
